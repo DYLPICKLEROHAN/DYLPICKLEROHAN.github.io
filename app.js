4ab7637b41d25efc7df198c454eb9fd3b1879eae
Dylan Rohan <100188169+DYLPICKLEROHAN@users.noreply.github.com>
2025-01-21 17:47:19 +1100

    Updating files

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 function alertButton() {
     alert("Hows that for functionality?");
-}
+};
+
+function hitchHikersGuide() {
+    alert('I have it under good authority that its 42, but keep it under your hat');
+};
+
+function time() {
+    let output = Date.now();
+    return output
+};
+
